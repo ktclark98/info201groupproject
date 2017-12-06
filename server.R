6b2d054a08ceb0d18532cd13520b0a654e396fb4
Tabitha-Anderson <andert97@uw.edu>
2017-12-06 08:55:33 -0800

    made changes so the histogram and the historical line plot are in the same spot

--- server.R
+++ server.R
@@ -1,88 +1,41 @@
 library(ggplot2)
-
 library(shiny)
 
-
-
 source("Tab's-Data-Wrangling.R")
-
 source("country-map.R")
-
 source("selected-country-data.R")
-
 source("overtime-graphic.R")
 
-
-
 shinyServer(function(input, output) {
+  # output$distPlot <- renderPlot({
+  #   # Displays the historial assessment graph if the checkbox is selected 
+  #   if (input$checkGroup == "historical") {
+  #     HistoricalAssessment(input$text) 
+  #   }
+  # })
   
-  
-  
-  output$distPlot <- renderPlot({
-    
-    # Displays the historial assessment graph if the checkbox is selected 
-    
-    if (input$checkGroup == "historical") {
-      
-      HistoricalAssessment(input$text) 
-      
-    }
-    
-  })
-  
-  
-  output$threatPlot <- renderPlot({
-    
+  output$histPlot <- renderPlot({
     if (input$checkGroup == "threats") {
-      
       plot <- ThreatHistogram(input$text)
-      
-    }
-    
-    plot
-    
-  })
-  
-  output$conservationPlot <- renderPlot({
-    
-    if (input$checkGroup == "action") {
-      
+    } else if (input$checkGroup == "action") {
       plot <- ConservationHistogram(input$text)
-      
+    } else if (input$checkGroup == "habitat") {
+      plot <- HabitatHistogram(input$checkGroup)
+    } else if (input$checkGroup == "historical") {
+        plot <- HistoricalAssessment(input$text) 
     }
-    
     plot
-    
-  })
-  
-  output$habitatPlot <- renderPlot({
-    
-    if (input$checkGroup == "habitat") {
-      
-      plot <- HabitatHistogram(input$text)
-      
-    }
-    
-    plot
-    
   })
   
   output$worldMap <- renderPlotly({
     
     p
-    
   })
-  
-  
   
   output$country.pie <- renderPlotly({
-    
     iso2 <- as.character(input$country)
-    
     GetPie(iso2)
-    
   })
-  
 })
 
 shinyUI(my.ui)
--- ui.R
+++ ui.R
@@ -1,112 +1,54 @@
-
 library(shiny)
-
 library(plotly)
-
 library(knitr)
-
-
-
 # Knit markdown file
-
 rmdfile <- c("about-us.Rmd")
-
 sapply(rmdfile, knit, quiet = TRUE)
-
-
-
-
 
 my.ui <- navbarPage(
   
   "Endangered Species",
-  
-  
-  
+
   tabPanel("Your Country",
-           
            sidebarLayout(
-             
              sidebarPanel(
-               
                textInput("country", label = h3("Enter Your Country's 2 Letter Code:"))
-               
              ),
-             
              mainPanel(
-               
                plotlyOutput('country.pie')
-               
              )
-             
            )
-           
   ),
-  
-  
-  
+
   tabPanel("Location",
-           
            mainPanel(
-             
              plotlyOutput("worldMap")
-             
            )
-           
   ),
-  
-  
   
   tabPanel("Species",
-           
            sidebarLayout(
-             
              sidebarPanel(
-               
                textInput("text", label = h3("Species"), value = "Enter Species Name Here..."),
-               
                checkboxGroupInput("checkGroup",
-                                  
                                   label = h3("Select Information"),
-                                  
                                   choices = list("Actions" = "action", "Threats" = "threats", "Habitat" = "habitat", "Historial Assessment" = "historical")
-                                  
                )
-               
              ),
-             
              mainPanel(
-               
-               plotOutput("distPlot"),
-               
-               plotOutput("threatPlot"),
-               
-               plotOutput("conservationPlot"),
-               
-               plotOutput("habitatPlot")
-               
+               #plotOutput("distPlot"),
+               plotOutput("histPlot")
              )
-             
            )
-           
   ),
   
-  
-  
   tabPanel("About Us",
-           
-           fluidPage(
-             
+           fluidPage (
              includeMarkdown("about-us.md")
-             
            )
-           
   )
-  
 )
 
 
-
 # Define UI for application that draws a histogram
-
 shinyUI(my.ui)
